579c135f30fcce9d385e37594e4d3ad68ebfaf68
OswaldoSantos <oswaldo@vps.fmvz.usp.br>
2013-07-29 21:07:17 -0300

    psukml: fixed bug for compatibility with sampling with replacement.

--- capm/R/psukml.R
+++ capm/R/psukml.R
@@ -1,9 +1,10 @@
-#' Creates a folder with *.kml files of a subset of polygons form a polygon shapefile
+#' Creates *.kml files of a subset of polygons form a polygon shapefile
 #' @description Subset polygons acording to the matches between a vector and a specified column from a \link[sp]{SpatialPolygonsDataFrame}.
 #' @param mp polygon shapefile layer name.
 #' @param psu the values to be matched.
 #' @param id column of the *.dbf file with the values to be matched against.
-#' @return A folder with *.kml files of the subsetted polygons.
+#' @return *.kml files of the subsetted polygons.
+#' @details If there is *.kml files in the working directory, the new created files will overwrite it in case of name matching.
 #' @seealso \code{\link{readShapeSpatial}}
 #' @export
 #' @examples
@@ -25,13 +26,19 @@ psukml <- function(mp = NULL, psu = NULL, id = NULL) {
   proj4string(tmp) <- CRS('+proj=longlat +ellps=WGS84')
   tmp2 = NULL
   for(i in 1:length(psu)) {
+    if (file.exists(paste(eval(psu[i]), '.kml', sep =''))) {
+      file.remove(paste(eval(psu[i]), '.kml', sep =''))
+    }
     tmp1 <- tmp[which(as.character(tmp@data[ , id]) == psu[i]), ]
     writeOGR(tmp1, dsn = paste(eval(psu[i]), '.kml', sep =''), 
              layer = 'selected_psu', driver = 'KML')
     tmp2[i] <- which(as.character(tmp@data[ , id]) == psu[i])
   }
   tmp2 <- tmp[tmp2, ]
+  if (file.exists('all_psu.kml')) {
+    file.remove('all_psu.kml')
+  }
   writeOGR(tmp2, dsn = 'all_psu.kml', 
-           layer = 'selected_psu', driver = 'KML')
+           layer = 'all_selected_psu', driver = 'KML')
   return(cat('\n', 'The maps are in the directory:', '\n\n', getwd()))
 }
