b83239fc81ff709e503ff074c595c9a89f8c8072
OswaldoSantos <oswaldo@vps.fmvz.usp.br>
2014-01-12 17:25:35 -0200

    DesignSurvey: documentation improvements.

--- capm/R/DesignSurvey.R
+++ capm/R/DesignSurvey.R
@@ -1,14 +1,14 @@
 #' Two-stage cluster sampling analysis
 #' @description A wraper for \code{\link{svydesign}} function from the survey package, to specify a two-stage cluster sampling analysis. Weights are calculated considering a probability proportional to size sampling with replacement for the first stage and a simple random sampling for the second stage. Finite population correction is specified as the population size for each level of sampling.
 #' @param psu.ssu \code{\link{data.frame}} with all primary sampling units (psu). First column contains psu unique identifiers. Second column contains \code{\link{numeric}} psu sizes.
-#' @param sample a \code{\link{data.frame}} with sample observations. One of the columns must contain unique identifiers for psu. Another column must contain unique identifiers for ssu. The rest of the columns corresponds to the variables surveyed.
+#' @param sample \code{\link{data.frame}} with sample observations. One of the columns must contain unique identifiers for psu. Another column must contain unique identifiers for ssu. The rest of the columns corresponds to the variables surveyed.
 #' @param psu.col the column of \code{sample} containing the psu identifiers.
 #' @param ssu.col the column of \code{sample} containing the ssu identifiers.
 #' @param design string to define the type of design. "2clusterPPS" defines a two-stage cluster sampling design with selection of PSU with probability proportional to size. "simple" defines a simple (systematic) random sampling design.
 #' @param psu.2cd value indicating the number of psu included in a design of type "2clusterPPS" (for psu included more than once, each must be counted).
 #' @param total value corresponding to population size. If \code{design} is equal to "2clusterPPS", it is not necessary to define \code{total}.
 #' @param ... further arguments passed to \code{\link{svydesign}} function. 
-#' @details A psu appearing in both \code{psu.ssu} and in \code{sample} must have the same identifier. ssu identifiers must be unique but can appear more than once if there is more than one observations per ssu.
+#' @details A psu appearing in both \code{psu.ssu} and in \code{sample} must have the same identifier. ssu identifiers must be unique but can appear more than once if there is more than one observation per ssu.
 #' @references Lumley, T. (2011). Complex surveys: A guide to analysis using R (Vol. 565). Wiley.
 #' @export
 #' @examples 
@@ -19,7 +19,7 @@
 #' data(Sample)
 #' 
 #' # Specify the two-stage cluster design.
-#' design = DesignSurvey(psu.ssu, Sample, psu.col = 2, ssu.col = 1)
+#' design <- DesignSurvey(psu.ssu, Sample, psu.col = 2, ssu.col = 1, psu.2cd = 20)
 
 DesignSurvey <- function(psu.ssu = NULL, sample = NULL, psu.col = NULL, ssu.col = NULL, design = '2clusterPPS', psu.2cd = NULL, total = NULL, ...) {
   if (design == '2clusterPPS') {
@@ -32,7 +32,6 @@ DesignSurvey <- function(psu.ssu = NULL, sample = NULL, psu.col = NULL, ssu.col 
     sample <- cbind(sample, pop.size)
     sample <- merge(sample, psu.ssu, by.x = psu.col, by.y = 1)
     names(sample)[ncol(sample)] <- 'psu.size'
-    #psu.size <- tapply(sample$psu.size, sample$psu.id, unique)
     psu.sample.size <- tapply(sample$psu.size, sample$psu.id, length)
     psu.sample.size <- rep(psu.sample.size, psu.sample.size)
     f.1 <- psu.2cd * sample$psu.size / sum(psu.ssu[, 2])
