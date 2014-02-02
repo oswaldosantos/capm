458fe93088004bbb30358b79f97faccb8036c76d
oswaldosantos <oswaldo@vps.fmvz.usp.br>
2014-02-02 14:57:24 -0200

    Delete print.capmModels.R

--- print.capmModels.R
+++
@@ -1,9 +0,0 @@
-print.capmModels <- function(x, ...) {
-  cat('\nModel function: ', x$name)
-  cat('\n\nParameters:\n\n')
-  print(x$pars)
-  cat('\nInitial conditions:\n\n')
-  print(x$init)
-  cat('\nInitial time: ', x$time[1])
-  cat('\n\nFinal time: ', x$time[length(x$time)])
-}
