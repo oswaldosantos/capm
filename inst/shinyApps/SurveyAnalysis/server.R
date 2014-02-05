7d63a7744f4bc5519d1c4fd35c7ca2f218287e0f
OswaldoSantos <oswaldo@vps.fmvz.usp.br>
2014-02-05 17:13:54 -0200

    SurveyAnalisis: graphic interface update.

--- inst/shinyApps/SurveyAnalysis/server.R
+++ inst/shinyApps/SurveyAnalysis/server.R
@@ -1,11 +1,117 @@
 shinyServer(function(input, output) {
   
-  foo <- function() {
+  Universe <- function() {
+    if (input$examples) {
+      data(psu.ssu)
+      return(psu.ssu)
+    } else if (is.null(input$psu.ssu)) {
+      return()
+    } else {
+      return(read.csv(input$universe$datapath,
+                      sep=input$sep,
+                      quote=input$quote,
+                      header = input$header))
+    }
+  }
+  
+  SampleData <- function() {
+    if (input$examples) {
+      data(Sample)
+      return(Sample)
+    } else if (is.null(input$sample)) {
+      return()
+    } else {
+      return(read.csv(input$sample$datapath,
+                      sep=input$sep,
+                      quote=input$quote,
+                      header = input$header))
+    }
+  }
+  
+  Pyramid <- function() {
+    if (is.null(SampleData()) | is.na(input$age.col) | is.na(input$sex.col)) {
+      return()
+    } else if (is.na(input$cas.col)) {
+      return(PlotPopPyramid(SampleData(), input$age.col,
+                            input$sex.col))
+    } else {
+      return(PlotPopPyramid(SampleData(), input$age.col,
+                            input$sex.col, input$cas.col))
+    }
+  }
+  
+  Design <- function() {
+    if (is.na(input$total)) {
+      if (is.null(Universe()) | is.null(SampleData()) |
+            is.na(input$psu.col) | is.na(input$ssu.col) |
+            is.na(input$psu.2cdl)) {
+        return()
+      } else {
+        return(DesignSurvey(Universe(), SampleData(),
+                            input$psu.col, input$ssu.col,
+                            psu.2cd = input$psu.2cdl))
+      }
+    } else {
+      return(DesignSurvey(Universe(), SampleData(),
+                          input$psu.col, input$ssu.col,
+                          design = 'simple',
+                          total = input$total))
+    }
   }
     
-  output$o1 <- renderTable({
-  })
-  
-  output$o2 <- renderPlot({
+    Variables <- function() {
+      if (is.null(input$variables)) {
+        return()
+      } else if (is.na(input$total)) {
+        no.spaces <- gsub(' +', '', input$variables)
+        unquoted <- unlist(strsplit(no.spaces, ','))
+        return(c('', '', unquoted, '', '', ''))
+      } else {
+        no.spaces <- gsub(' +', '', input$variables)
+        unquoted <- unlist(strsplit(no.spaces, ','))
+        return(c('', '', unquoted, ''))
+      }
+    }
+    
+    output$universe <- renderDataTable({
+      Universe()
+    }, options = list(aLengthMenu = c(5, 30, 50), iDisplayLength = 5))
+    
+    output$sample <- renderDataTable({
+      SampleData()
+    }, options = list(aLengthMenu = c(5, 30, 50), iDisplayLength = 5))
+    
+    output$summ.sample <- renderPrint({
+      if (!is.null(SampleData())) {
+        summary(SampleData())
+      }
     })
+    
+    output$pyramid <- renderPlot({
+      Pyramid()
+    })
+    
+    output$variables <- renderTable({
+      if (is.null(Design()) | is.null(Variables())) {
+        return()
+      } else {
+        des.var <- names(Design()$variables)
+        cbind(Variables = des.var, 'Tipe of estimate' = Variables())[
+          -c(1:2, (length(des.var) - 2):length(des.var)), ]
+      }
+    })
+    
+    output$estimates <- renderTable({
+      if (input$calc.estimates == 0) {
+        return()
+      }
+      isolate({
+        if (is.null(Design()) | is.null(Variables())) {
+          return()
+        } else {
+          SummarySurvey(Design(), Variables())
+        }
+      })
+    })
+    
   })
--- inst/shinyApps/SurveyAnalysis/ui.R
+++ inst/shinyApps/SurveyAnalysis/ui.R
@@ -6,7 +6,7 @@ shinyUI(pageWithSidebar(
     
     conditionalPanel(
       condition = 'input.conditionedPanels == 1',
-      helpText('Intro')),
+      checkboxInput('examples', 'Use example files from capm package instead of your own data.', F)),
     
     conditionalPanel(
       condition = 'input.conditionedPanels == 2',
@@ -22,7 +22,7 @@ shinyUI(pageWithSidebar(
                      'Double Quote'='"',
                      'Single Quote'="'"),
                    'Double Quote'),    
-      fileInput('psu.ssu', '',
+      fileInput('universe', '',
                 accept=c('text/csv',
                          'text/comma-separated-values,text/plain',
                          '.csv'))),
@@ -48,43 +48,40 @@ shinyUI(pageWithSidebar(
     
     conditionalPanel(
       condition = 'input.conditionedPanels == 4',
-      helpText('Survey design'),
+      helpText('Population pyramid.'),
+      numericInput('age.col',
+                   'Column in sample data containing age variable.',
+                   value = NULL, min = 0),
+      numericInput('sex.col',
+                   'Column in sample data containing sex variable.',
+                   value = NULL, min = 0),
+      numericInput('cas.col',
+                   'Column in sample data containing reproductive status variable.',
+                   value = NULL, min = 0)
+    ),
+    
+    conditionalPanel(
+      condition = 'input.conditionedPanels == 5',
       numericInput('psu.col',
-                   'Column in sample data csv that contain PSU identifiers.',
+                   'Column in sample data that contain PSU identifiers.',
                    value = NULL, min = 0),
       numericInput('ssu.col',
-                   'Column in sample data csv that contain SSU identifiers.',
+                   'Column in sample data that contain SSU identifiers.',
                    value = NULL, min = 0),
       numericInput('psu.2cdl',
                    'Number  of PSU included in the design.',
                    value = NULL, min = 0),
-      numericInput('systematic',
-                   'For systematic sampling, ingore the previous three fields and specify here the total number of sampling units in the population.',
-                   value = NULL, min = 0)),
-    
-    conditionalPanel(
-      condition = 'input.conditionedPanels == 5',
-      helpText('Population pyramid.'),
-      numericInput('age.col',
-                   'Column in sample data csv containing age variable.',
-                   value = NULL, min = 0),
-      numericInput('sex.col',
-                   'Column in sample data csv containing sex variable.',
-                   value = NULL, min = 0),
-      numericInput('cas.col',
-                   'Column in sample data csv containing reproductive status variable.',
-                   value = NULL, min = 0),
-      
       tags$hr(),
-      checkboxInput('examples', 'Instead of choosing your own csv files, use example files from capm package. After checking this box, press the "Get map" above.', F)
-      
-    ),
-    
-    conditionalPanel(
-      condition = 'input.conditionedPanels == 6',
-      helpText('Population pyramid.'))
-    
-    ),
+      HTML('<p> Type of estimates <br> If you are using example files, copy and paste the following terms: <br> total, prop, mean, prop, prop, prop, total, prop, prop, prop'),
+      textInput('variables', '',
+                 value = NULL),
+      tags$hr(),
+      numericInput('total',
+                   'For systematic sampling, ingore the first three fields and specify here the total number of sampling units in the population.',
+                   value = NULL, min = 0),
+      tags$hr(),
+      actionButton('calc.estimates', 'Get estimates'))
+  ),
   
   mainPanel(
     tabsetPanel(
@@ -93,33 +90,53 @@ shinyUI(pageWithSidebar(
         'Introduction', value = 1,
         HTML(
           '<p>
-          bla, : <br>
-          <ul>
-          <li> bla. </li>
-          <li> bla. </li>
-          </ul>
+          With this App, you can upload data collected in a survey, make some descriptive analysis, and estimate population parameters. To get the estimates, it is necessary to specify the survey design used to collect the data.<br><br>
+
+<b>Runing example files</b><br>
+To run the App with example files from capm package instead of your own data, check the box in the left side panel.<br><br>
+
+<b>Universe of sampling units</b><br>
+If you checked the box to use example files, ignore the left side panel of the <i>Universe of sampling units</i> Tab. If you are using your own files, make sure you choose the apropriate options in the left side panel to avoid error messages or awkward results. You must be able to see a table with two columns and as many rows as sampling units in the population.<br><br>
+
+<b>Survey data</b><br>
+If you checked the box to use example files, ignore the left side panel of the <i>Survey data</i> Tab.  If you are using your own files, make sure you choose the apropriate options in the left side panel to avoid error messages or awkward results. The csv file must have one column for each variable for which estimates are needed, one column for PSU identifiers and one column for SSU identifiers. You must be able to see that columns and as many rows as SSU (across all PSU) in the sample. On the top of the table, the gray box shows summary statistics for each column. In the example file, the first column contain SSU identifiers, the second contain PSU identifiers and the remaining contain the variables.<br>
+To see a description of the meaning of each variable, go to RStudio and run <code>help(Sample)</code>.<br><br>
+
+<b>Population pyramid</b><br>
+If you checked the box to use example files, fill in the fields from top to bottom with, 5, 4 and 6 respectively (see this column positions in the output from <i>Survey data</i> Tab).<br><br>
+
+<b>Estimates</b><br>
+If you checked the box to use example files, fill in the first three fields from top to bottom with, 2, 1 and 20 respectively (see the first two columns in the output from <i>Survey data</i> Tab). The example survey design included 20 PSU and that is the reason for the last number.<br>
+For systematic sampling, ignore the first three fields, and specify the total number of sampling units in the population (fourth field).<br>
+In the last field, specify the type of estimate for each variable. To estimate a total, type <i>total</i>, to estimate a mean, type <i>mean</i> and to estimate a proportion, type <i>prop</i>. To get estimates of more than one variable, type the appropriate term for each variable and separate them by commas. The order of term from left to right must be the order (left to right) of variables (columns) in the survey file.<br>
+From the command line in RStudio, it is possible to get estimates for specific subpopulations (i.e. by sex). To estimate the total for categorical variables such as "castrated" from the example file (<i>Survey data</i> Tab), the command line is the place to go.<br><br>
+
+<b>Further information</b><br>
+<ul>
+<li>The display of some outputs might take a few seconds, be patient!</li>
+<li>Reload the page to reset the fields</li>
+<li>Working from command line, you will have more options and flexibility.</li>
+<li>Torials with more detailed information can be found in <a href="https://github.com/oswaldosantos/capm">https://github.com/oswaldosantos/capm</a></li>
+<li>If you find errors, have suggestions or any question, I will be glad to know <a href="mailto:oswaldosant@gmail.com">oswaldosant@gmail.com</a></li>
+</ul>
           </p>
-          <p>
-          bla.
-          </p>
-          ')
-      ),
+          ')),
       
-      #tabPanel('Universe of sampling units', value = 2,
-       #        tableOutput('universe')),
+      tabPanel('Universe of sampling units', value = 2,
+               dataTableOutput('universe')),
       
-      tabPanel('Sample data', value = 3,
-               HTML('bla'),
-               tableOutput('sample')),
+      tabPanel('Survey data', value = 3,
+               verbatimTextOutput('summ.sample'),
+               dataTableOutput('sample')),
       
-      tabPanel('Survey design', value = 4,
-               tableOutput('pyramid')),
-      
-      tabPanel('Population pyramid', value = 5,
+      tabPanel('Population pyramid', value = 4,
                plotOutput('pyramid', height = 600)),
       
-      tabPanel("Estimates", value = 6,
-               tableOutput('estimatess')),
+      tabPanel('Estimates', value = 5,
+               tableOutput('variables'),
+               tags$hr(),
+               tableOutput('estimates')),
+      
       id = "conditionedPanels"
     )
   )
