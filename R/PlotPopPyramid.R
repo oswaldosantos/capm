8e6bdc62f2d4d3ac484742cfaedd8dfb8c0d24c7
OswaldoSantos <oswaldo@vps.fmvz.usp.br>
2014-02-05 17:23:51 -0200

    Documentation improvements.

--- R/PlotPopPyramid.R
+++ R/PlotPopPyramid.R
@@ -1,20 +1,20 @@
 #' Population PlotPopPyramid
 #' @description Dysplays two opposed horizontal barplots (pyramid).
 #' @param dat \code{\link{data.frame}}.
-#' @param col.age the number or name of \code{dat} column which have a \code{\link{numeric}} \code{\link{vector}} representing ages or stage categories.
-#' @param col.sex the number or name of \code{dat} column which have a \code{\link{factor}} with two levels representing the sex of individuals (see Details).
-#' @param col.cas the number of \code{dat} column which have a \code{\link{factor}} representing the reproductive status of individuals (see Details).
+#' @param age.col the number or name of \code{dat} column which have a \code{\link{numeric}} \code{\link{vector}} representing ages or stage categories.
+#' @param sex.col the number or name of \code{dat} column which have a \code{\link{factor}} with two levels representing the sex of individuals (see Details).
+#' @param str.col the number of \code{dat} column which have a \code{\link{factor}} representing the reproductive status of individuals (see Details).
 #' @param x.label a string to be used as a label for x-axis. If non defined, \code{xlabel} is equal to "Total" (see Details).
 #' @param stage.label a string to be used as a label for ages or stages categories. If non defined, \code{stage.label} is equal to "Years" (see Details).
-#' @param inner.color any valid way to specify colors. When \code{col.cas} is \code{NULL}, \code{inner.color} is the color of bars. When \code{col.cas} is not \code{NULL}, \code{innercolor} is the inner color of bars. If non defined, \code{inner.color} is equal to "Gold2".
-#' @param outer.color any valid way to specify colors. When \code{col.cas} is \code{NULL}, \code{outer.color} is ignored. When \code{col.cas} is not \code{NULL}, \code{outer.color} is the outer color of bars. If non defined, \code{outercolor} is equal to "DarkOliveGreen".
+#' @param inner.color any valid way to specify colors. When \code{str.col} is \code{NULL}, \code{inner.color} is the color of bars. When \code{str.col} is not \code{NULL}, \code{innercolor} is the inner color of bars. If non defined, \code{inner.color} is equal to "Gold2".
+#' @param outer.color any valid way to specify colors. When \code{str.col} is \code{NULL}, \code{outer.color} is ignored. When \code{str.col} is not \code{NULL}, \code{outer.color} is the outer color of bars. If non defined, \code{outercolor} is equal to "DarkOliveGreen".
 #' @details \code{PlotPopPyramid} is mainly intended for companion animals population pyramids, although it can display other types of opposed bar charts with suitable modification of the arguments.
 #' 
-#' The bars to the left of \code{0} on the x axis correspond to the minimum value of \code{\link{as.numeric}}(\code{dat[, col.sex]}). If \code{col.cas} is not \code{NULL}, bars will be stacked, with the minimum value of \code{\link{as.numeric}}(\code{dat[, col.cas]}) as their base.
+#' The bars to the left of \code{0} on the x axis correspond to the minimum value of \code{\link{as.numeric}}(\code{dat[, sex.col]}). If \code{str.col} is not \code{NULL}, bars will be stacked, with the minimum value of \code{\link{as.numeric}}(\code{dat[, str.col]}) as their base.
 #' 
-#' On the top of the plot, it is displayed the total number of observations of each level of \code{dat[, col.sex]}. The \code{\link{levels}} of \code{col.sex} are used as \code{\link{labels}}.
+#' On the top of the plot, it is displayed the total number of observations of each level of \code{dat[, sex.col]}. The \code{\link{levels}} of \code{sex.col} are used as \code{\link{labels}}.
 #' 
-#' The legend title is equal to \code{names(dat[, col.cas])} and the legend \code{\link{labels}} are equal to the \code{\link{levels}} of \code{dat[, col.cas]}.
+#' The legend title is equal to \code{names(dat[, str.col])} and the legend \code{\link{labels}} are equal to the \code{\link{levels}} of \code{dat[, str.col]}.
 #' 
 #' Font size of saved plots is usually different to the font size seen in graphic browsers. Before changing font sizes, see the final result in saved (or preview) plots.
 #'  
@@ -26,25 +26,25 @@
 #' @examples 
 #' ## Load data with information about age, sex and reproductive status of individuals.
 #' data(survey.data)
-#' PlotPopPyramid(survey.data, col.age = 5, col.sex = 4, col.cas = 6)
-#' PlotPopPyramid(survey.data, col.age = 5, col.sex = 4)
+#' PlotPopPyramid(survey.data, age.col = 5, sex.col = 4, str.col = 6)
+#' PlotPopPyramid(survey.data, age.col = 5, sex.col = 4)
 #' 
-PlotPopPyramid <- function(dat = NULL, col.age = NULL, col.sex = NULL, col.cas = NULL, x.label = 'Total', stage.label = 'Years', inner.color = 'Gold2', outer.color = 'DarkOliveGreen') {
-  if (!is.numeric(dat[, col.age])) {
+PlotPopPyramid <- function(dat = NULL, age.col = NULL, sex.col = NULL, str.col = NULL, x.label = 'Total', stage.label = 'Years', inner.color = 'Gold2', outer.color = 'DarkOliveGreen') {
+  if (!is.numeric(dat[, age.col])) {
     stop('The column containing age information must be numeric.')
   }
-  if (!is.null(col.cas)) {
-    if (is.numeric(col.cas)) {
-      col.cas <- names(dat)[col.cas]
+  if (!is.null(str.col)) {
+    if (is.numeric(str.col)) {
+      str.col <- names(dat)[str.col]
     }
-    dat2 <- aggregate(dat, list(dat[, col.age],
-                                dat[, col.sex],
-                                dat[, col.cas]),
+    dat2 <- aggregate(dat, list(dat[, age.col],
+                                dat[, sex.col],
+                                dat[, str.col]),
                       length)
     names(dat2) <- c('age', 'sex', 'cas', 'count')
   } else {
-    dat2 <- aggregate(dat, list(dat[, col.age],
-                                dat[, col.sex]),
+    dat2 <- aggregate(dat, list(dat[, age.col],
+                                dat[, sex.col]),
                       length)
     names(dat2) <- c('age', 'sex', 'count')
   }
@@ -59,7 +59,7 @@ PlotPopPyramid <- function(dat = NULL, col.age = NULL, col.sex = NULL, col.cas =
   dat.m <- dat2[which(dat2[, 2] == levels(dat2$sex)[2]), ]
   dat.m <- rbind(dat.m, c(max(dat2$age), rep(0, ncol(dat2) - 1)))
   options(warn = -1)
-  if (!is.null(col.cas)) {
+  if (!is.null(str.col)) {
     plot.f <- ggplot(dat.f, aes(x = age, y = count , fill = cas)) +
       scale_fill_manual(values = c(inner.color, outer.color))
     plot.m <- ggplot(dat.m, aes(x = age, y = count , fill = cas)) +
@@ -82,8 +82,8 @@ PlotPopPyramid <- function(dat = NULL, col.age = NULL, col.sex = NULL, col.cas =
                        labels = 0:max(dat2$age)) +
     scale_y_continuous(breaks = seq(0, (ylb * -1), by = ylb / -5),
                        labels = seq(0, ylb, by = ylb / 5)) +
-    ggtitle(paste(levels(dat[, col.sex])[1], ' = ',
-                  summary(dat[, col.sex])[1])) +
+    ggtitle(paste(levels(dat[, sex.col])[1], ' = ',
+                  summary(dat[, sex.col])[1])) +
     ylab(x.label)
   plot.m <- plot.m +
     geom_bar(stat = 'identity') + coord_flip() +
@@ -99,11 +99,11 @@ PlotPopPyramid <- function(dat = NULL, col.age = NULL, col.sex = NULL, col.cas =
                        labels = 0:max(dat2$age)) +
     scale_y_continuous(breaks = seq(0, ylb, by = ylb / 5),
                        labels = seq(0, ylb, by = ylb / 5)) +
-    ggtitle(paste(levels(dat[, col.sex])[2], ' = ',
-                  summary(dat[, col.sex])[2])) +
-    labs(fill = col.cas) +
+    ggtitle(paste(levels(dat[, sex.col])[2], ' = ',
+                  summary(dat[, sex.col])[2])) +
+    labs(fill = str.col) +
     ylab(x.label)
-  if (is.null(col.cas)) {
+  if (is.null(str.col)) {
     plot.m <- plot.m + theme(legend.position = 'none')
   }
   ages <- ggplot(data.frame(age = 0:max(dat2$age), count = 0),
