1e1048ccc313b65c8d746d8f343278eaa0e84133
oswaldo <oswaldosant@gmail.com>
2016-04-14 11:37:02 -0300

    Selective imports

--- R/capm-package.R
+++ R/capm-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab capm\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.9.0\cr
-#' Date: \tab 2015-12-05\cr
+#' Version: \tab 0.9.1\cr
+#' Date: \tab 2016-03-06\cr
 #' Depends: \tab R (>= 3.2.2)\cr
 #' Imports: \tab deSolve, FME, survey, reshape2, ggplot2, shiny, grid, rgdal, maptools, sp\cr
 #' License: \tab GPL (>= 2)\cr
@@ -19,8 +19,11 @@
 #' @docType package
 #' @title The capm Package
 #' @keywords package
-#' @import deSolve FME survey reshape2 ggplot2 shiny rgdal maptools sp
+#' @import deSolve FME survey reshape2 ggplot2 shiny
+#' @importFrom rgdal readOGR writeOGR
 #' @importFrom grid viewport pushViewport grid.newpage grid.layout
 #' @importFrom stats aggregate confint qnorm runif sd var
 #' @importFrom utils write.table
+#' @importFrom maptools nowrapSpatialLines
+#' @importFrom sp spTransform CRS
 NULL
