45e5c2dafe44f42fb7c3e9fd0e4f7c4dff833f8b
oswaldo <oswaldosant@gmail.com>
2017-05-09 15:37:02 -0300

    Version upgrade

--- R/capm-package.R
+++ R/capm-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab capm\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.10.3\cr
-#' Date: \tab 2017-05-04\cr
+#' Version: \tab 0.11.0\cr
+#' Date: \tab 2017-05-05\cr
 #' Depends: \tab R (>= 3.4.0)\cr
 #' Imports: \tab deSolve, FME, survey, reshape2, ggplot2, shiny, grid, rgdal, maptools, sp\cr
 #' License: \tab GPL (>= 2)\cr
