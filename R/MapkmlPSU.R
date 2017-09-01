1bf0926cf61e0cbe0842c9eedba3f044d11c9bf7
oswaldosantos <baquero@usp.br>
2017-09-01 18:57:12 -0300

    MapkmlPSU: updated example.

--- R/MapkmlPSU.R
+++ R/MapkmlPSU.R
@@ -16,7 +16,7 @@
 #' (selected_psu <- SamplePPS(psu.ssu = city[, c("track_id", "hh")], psu = 10))
 #' 
 #' ## Define shape from shapefile.
-#' shp_path <- system.file("extdata", package="capm")
+#' shp_path <- system.file("extdata/35SEE250GC_SIR.shp", package="capm")
 #' # The code above used a shapefile avaliable in the
 #' # capm package.
 #' # You might want to write a code like:
@@ -24,7 +24,7 @@
 #' 
 #' # Create *kml files of 10 polygons.
 #' \dontrun{
-#' MapkmlPSU(shape = "35SEE250GC_SIR",
+#' MapkmlPSU(shape = "shp_path",
 #'           psu = selected_psu[, "selected_psu"],
 #'           id = "CD_GEOCODI")
 #' 
@@ -35,7 +35,7 @@ MapkmlPSU <- function(shape = NULL, psu = NULL, id = NULL) {
   if (any(class(shape) == 'sf')) {
     tmp <- shape
   } else {
-    tmp <- st_read(shape)
+    tmp <- st_read(shape, quiet = TRUE)
   }
   tmp <- st_transform(tmp, 4326)
   tmp2 = NULL
