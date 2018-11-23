bb19d2ef26aa08ef712c70dc7e757ea74314d984
oswaldosantos <baquero@usp.br>
2018-11-23 18:50:56 -0200

    MapkmlPSU: bug fixed in the argument "path"

--- R/MapkmlPSU.R
+++ R/MapkmlPSU.R
@@ -3,6 +3,7 @@
 #' @param shape string with the path of a polygon shapefile or an object of \code{\link{class}} \link[sf]{sf} (see examples).
 #' @param psu polygons to subset.
 #' @param id column of the *.dbf file with the values to be matched against.
+#' @param path \code{\link{class}} character with the path in which maps will be saved. If the path already exists, it will be overwrited.
 #' @return *.kml files of the subsetted polygons.
 #' @details If there are *.kml files in the working directory, the new created files will overwrite it in case of name matching.
 #' 
@@ -35,7 +36,7 @@
 #' MapkmlPSU(shape = x, psu = selected_psu[, "selected_psu"], id = "CD_GEOCODI")
 #' }
 #' 
-MapkmlPSU <- function(shape = NULL, psu = NULL, id = NULL) {
+MapkmlPSU <- function(shape = NULL, psu = NULL, id = NULL, path = "./psu_maps/") {
   if (any(class(shape) == 'sf')) {
     tmp <- shape
   } else {
@@ -43,18 +44,26 @@ MapkmlPSU <- function(shape = NULL, psu = NULL, id = NULL) {
   }
   tmp <- st_transform(tmp, 4326)
   tmp2 = NULL
+  if (dir.exists(path)) {
+    unlink(path, recursive = TRUE)
+  }
+  dir.create(path)
+  path <- normalizePath(path)
   for(i in 1:length(psu)) {
     tmp1 <- tmp[which(as.character(tmp[[id]]) == psu[i]), ]
-    st_write(tmp1, dsn = paste(eval(psu[i]), '.kml', sep =''), 
+    if (file.exists(paste(path, eval(psu[i]), '.kml', sep =''))) {
+      file.remove(paste(path, eval(psu[i]), '.kml', sep =''))
+    }
+    st_write(tmp1, dsn = paste(path, "/", eval(psu[i]), '.kml', sep =''), 
              layer = 'selected_psu', driver = 'KML', delete_layer = TRUE,
              quiet = TRUE)
     tmp2[i] <- which(as.character(tmp[[id]]) == psu[i])
   }
   tmp2 <- tmp[tmp2, ]
-  if (file.exists('all_psu.kml')) {
-    file.remove('all_psu.kml')
+  if (file.exists(paste0(path, '/all_psu.kml'))) {
+    file.remove(paste0(path, '/all_psu.kml'))
   }
-  st_write(tmp2, dsn = 'all_psu.kml', layer = 'all_selected_psu',
+  st_write(tmp2, dsn = paste0(path, '/all_psu.kml'), layer = 'all_selected_psu',
            driver = 'KML', delete_layer = TRUE, quiet = TRUE)
-  return(cat('\n', 'The maps are in the directory:', '\n\n', getwd()))
+  return(cat('\n', 'The maps are in the directory:', '\n\n', path))
 }
--- R/capm-package.R
+++ R/capm-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab capm\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.13.7\cr
-#' Date: \tab 2018-11-13\cr
+#' Version: \tab 0.13.9\cr
+#' Date: \tab 2018-11-23\cr
 #' Depends: \tab R (>= 3.4)\cr
 #' Imports: \tab deSolve, FME, survey, dplyr, tidyr, magrittr, ggplot2, grid, stats, utils, sf\cr
 #' License: \tab GPL (>= 2)\cr
