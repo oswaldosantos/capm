a5e1b83eea6c8c6be09641ae22d66bdbb22075d2
oswaldosantos <baquero@usp.br>
2019-04-22 17:42:09 -0300

    Fix date format

--- R/capm-package.R
+++ R/capm-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab capm\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.13.10\cr
-#' Date: \tab 2019-18-04\cr
+#' Date: \tab 2019-04-19\cr
 #' Depends: \tab R (>= 3.4)\cr
 #' Imports: \tab deSolve, FME, survey, dplyr, tidyr, magrittr, ggplot2, grid, stats, utils, sf\cr
 #' License: \tab GPL (>= 2)\cr
